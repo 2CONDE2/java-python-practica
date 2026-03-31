9275e646bc8c81f5557c420618866321a5002077
Jheron Dylan Conde Martinez <jheronconde032@gmail.com>
2026-03-31 08:36:24 -0400

    ejercisios where

--- sql_server/ejercisios.sql
+++ sql_server/ejercisios.sql
@@ -1,4 +1,5 @@
 Nivel 1 — SELECT básico
+
 1️⃣ Mostrar todos los empleados.
 
 SELECT *
@@ -25,3 +26,47 @@ WHERE Edad > 50
 SELECT *
 FROM Empleados
 WHERE Edad < 40
+
+Nivel 2 — WHERE
+
+6️⃣ Mostrar los empleados que trabajan en la empresa 1001.
+
+SELECT *
+FROM Empleados
+WHERE codEmpresa = 1001
+
+7️⃣ Mostrar los empleados que tienen entre 40 y 50 años.
+
+SELECT *
+FROM Empleados
+WHERE Edad > 40 AND Edad < 50
+
+8️⃣ Mostrar los empleados mayores de 45 años.
+
+SELECT * 
+FROM Empleados
+WHERE Edad > 45
+
+9️⃣ Mostrar nombre y edad de los empleados de la empresa 1002.
+
+SELECT Nombre, Edad 
+FROM Empleados
+WHERE codEmpresa = 1002
+
+10️⃣ Mostrar los empleados cuyo nombre sea "Maria".
+
+SELECT *
+FROM Empleados
+WHERE Nombre = 'Maria'
+
+Nivel 3 — JOIN
+
+11️⃣ Mostrar nombre del empleado y nombre de la empresa donde trabaja.
+
+12️⃣ Mostrar nombre, edad y empresa de cada empleado.
+
+13️⃣ Mostrar los empleados mayores de 50 años junto con su empresa.
+
+14️⃣ Mostrar empleados que trabajan en TechNova.
+
+15️⃣ Mostrar nombre del empleado y su empresa ordenados por empresa.
