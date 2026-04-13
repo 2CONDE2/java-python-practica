bf15b9ba2979546d7080d9d8a862767b6f65f4ae
Jheron Dylan Conde Martinez <jheronconde032@gmail.com>
2026-04-13 15:35:14 -0400

    metodo es primo

--- Java/metodosBasicosRecursividad.java
+++ Java/metodosBasicosRecursividad.java
@@ -2,7 +2,12 @@ public class metodosBasicosRecursividad {
     public static void main(String[] args){
 
     int a= 2; int b = 5;
-     System.out.print(potencia(a,b)) ;
+     System.out.println(potencia(a,b)) ;
+
+     if(esPrimo(b))
+        System.out.println(b+": es primo ");
+     else
+        System.out.println("no es primo");
     }
 
     private static int potencia (int b, int p){
@@ -13,5 +18,23 @@ public class metodosBasicosRecursividad {
         res= b * potencia(b,p-1); 
 
     return res;
+    }
+    private static boolean esPrimo(int n){
+     
+    return esPrimo(n,2);
+
+    }
+    private static boolean esPrimo(int n, int divisor) {
+        boolean res = false ;
+        if (n <= 1) 
+            res = false;
+        if (divisor == n) 
+            res = true;
+        if (n % divisor == 0) 
+            res = false;
+        else
+           esPrimo(n, divisor + 1);
+
+        return res;
     }
 }
