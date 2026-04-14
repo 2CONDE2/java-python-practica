8e74b481f6842d4ec0d8cb3ea2cde0792aca04e1
Jheron Dylan Conde Martinez <jheronconde032@gmail.com>
2026-04-14 13:54:14 -0400

    metodo para sumar los elementos de la secuencia

--- Java/Secuencia.java
+++ Java/Secuencia.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class Secuencia {
+  
   public static void main(String[] args) {
     Random rnd = new Random();
     ArrayList<Integer> secuencia= new ArrayList<>(); 
@@ -18,9 +19,29 @@ public class Secuencia {
 
             for(int i : secuencia){
                  
-               System.err.print(" "+i);
-            }
+               System.out.print(" "+i);
+            
+              }
+              System.err.println();
+
+    System.out.println("la suma de los elementos es " + suma(secuencia));       
+
+  }    
+
+  private static int suma(ArrayList<Integer> lista){
+       
+      return suma(lista , 0);
+
   }
 
-    
+  private static int suma(ArrayList<Integer> lista, int tam) {
+        int suma ;
+        if (tam == lista.size()) 
+            suma = 0;
+
+        else 
+        suma= lista.get(tam) + suma(lista, tam + 1);
+
+        return suma;
+    }
 }
